ee087baecd506d41cac5fc3fbdd666b11461a93a
Dilshadaziz00 <dilshadaziz461@gmail.com>
2024-07-10 18:44:28 +0500

    project

---
+++ assets/js/axios-config.js
@@ -0,0 +1,31 @@
+// axios-config.js
+
+import axios from 'axios';
+
+axios.defaults.baseURL= 'http://192.168.100.20:8000';
+
+// Add a request interceptor
+axios.interceptors.request.use(
+  function(config) {
+    // Do something before request is sent
+    return config;
+  },
+  function(error) {
+    // Do something with request error
+    return Promise.reject(error);
+  }
+);
+
+// Add a response interceptor
+axios.interceptors.response.use(
+  function(response) {
+    // Do something with response data
+    return response;
+  },
+  function(error) {
+    // Do something with response error
+    return Promise.reject(error);
+  }
+);
+
+
